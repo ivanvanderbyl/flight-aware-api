27506f41f0dfeca38935f22cc7be5d7612ff6517
Ivan Vanderbyl <ivanvanderbyl@gmail.com>
2019-12-20 07:48:11 +0800

    Fix strict typings on api call

--- src/FlightAware.ts
+++ src/FlightAware.ts
@@ -1,56 +1,38 @@
 import fetch from 'isomorphic-unfetch'
 import { stringify } from 'query-string'
 import {
+  FlightAwareData,
   FlightInfoExResult,
   AirlineFlightInfoStruct,
   AirlineInfoResult,
   AllAirlinesResult,
 } from './types/FlightAware'
 
-export type APIMethod =
-  | 'FlightInfo'
-  | 'FlightInfoEx'
-  | 'AirlineFlightInfo'
-  | 'AirlineInfo'
-  | 'AllAirlines'
-
 const API_URL = 'http://flightxml.flightaware.com/json/FlightXML2'
+
+type Methods = 'FlightInfoExResult' | string
+type FlightAwareResponse<T> = Record<string, T>
 
 export default class FlightAware {
   constructor(private username: string, private apiKey: string) {}
 
-  public async flightInfo(ident: string): Promise<FlightInfoExResult> {
+  public async flightInfo(ident: string): Promise<FlightInfoExResult | null> {
     return this.apiCall<FlightInfoExResult>('FlightInfoEx', { ident })
   }
 
-  public async airlineFlightInfo(faFlightID: string): Promise<AirlineFlightInfoStruct> {
+  public async airlineFlightInfo(faFlightID: string): Promise<AirlineFlightInfoStruct | null> {
     return this.apiCall<AirlineFlightInfoStruct>('AirlineFlightInfo', { faFlightID })
   }
 
-  public async airlineInfo(airlineCode: string): Promise<AirlineInfoResult> {
+  public async airlineInfo(airlineCode: string): Promise<AirlineInfoResult | null> {
     return this.apiCall<AirlineInfoResult>('AirlineInfo', { airlineCode })
   }
 
-  public async allAirlines(): Promise<AllAirlinesResult> {
+  public async allAirlines(): Promise<AllAirlinesResult | null> {
     return this.apiCall<AllAirlinesResult>('AllAirlines', {})
   }
 
-  /**
-   * This endpoint costs ~$10 to call, be careful.
-   */
-  public async detailedAirlineInfo() {
-    let airlines = await this.allAirlines()
-    let list = []
-    for (const airlineCode of airlines.data) {
-      console.log(airlineCode)
-      let data = await this.airlineInfo(airlineCode)
-      list.push({ ...data, airlineCode })
-    }
-
-    return list
-  }
-
-  private async apiCall<T>(method: APIMethod, args: any): Promise<T> {
+  private async apiCall<T>(method: string, args: any): Promise<T | null> {
     let queryString = stringify(args)
     return fetch(`${API_URL}/${method}?${queryString}`, {
       credentials: 'include',
@@ -58,15 +40,17 @@ export default class FlightAware {
         Authorization: `Basic ${Buffer.from(`${this.username}:${this.apiKey}`).toString('base64')}`,
       },
     })
-      .then(res => {
+      .then<FlightAwareResponse<T> | null>(res => {
         if (res.ok) {
-          return res.json()
+          return res.json() as PromiseLike<FlightAwareResponse<T>>
         } else {
-          return {}
+          return null
         }
       })
       .then(data => {
-        return data[`${method}Result`]
+        let resultKey = `${method}Result`
+        if (data && resultKey in (data as any)) return data[resultKey]
+        return null
       })
   }
 }
